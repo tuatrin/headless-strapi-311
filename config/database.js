37364c18c1eb9e5e6aef9d18f5ae94c6e8e8781a
tuatrin <tuatrin@gmail.com>
2023-12-25 14:49:18 +0700

    add document root for node js plesk configuration

--- config/database.js
+++ config/database.js
@@ -12,4 +12,17 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  mysql: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      options: {}
+    }
 });
