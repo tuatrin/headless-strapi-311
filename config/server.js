dcf5f8b96d386255f4d0621aee3c351281d48008
tuatrin <tuatrin@gmail.com>
2023-12-25 14:42:38 +0700

    initial strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5bf4c8199db2a4c7c165468688ee2608'),
+    },
+  },
+});
